b55c35bd2c8566e7aac6edcca0caa41c3e60d0eb
class12 <mansoorihassan409@gmail.com>
2025-06-26 10:41:28 +0500

    class1 completed

---
+++ Class -01/app.js
@@ -0,0 +1,57 @@
+// Datatypes
+
+// string
+// "" ''
+
+// number
+// 23 0.4
+
+
+// built-in function
+// console.log();
+// alert()
+// prompt()
+
+
+// variables
+// let const
+
+// let lastName;
+// lastName= "Ahmed";
+
+
+// Arthmatic operators
+// + - * / %
+// sum, concatination
+
+// BODMAS
+
+// let firstName = "Talal";
+    //  4    6    4    2    1
+// const num = 2 - 2 + 2 + 2 * 2 / 2;
+
+
+//             28       1      2      26
+// let sum = num1 + num2 / num2 * num1 + num2;
+
+
+// console.log(firstName);
+
+// alert("registration successful!");
+
+
+// let fullName = prompt("What is your name?");
+// let occupation = prompt("What is your occupation?");
+
+// console.log("Welcome! " + fullName + " your occupation is: " + occupation);
+
+//             26     34     8
+// let sum = (num1 + num2) + (2 * 4);
+
+// let num1 = 2;
+
+// let num2 = 3;
+
+// let sum = num1 % num2
+
+// console.log(sum);
