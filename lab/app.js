3eba40e8012fbfeca69f963bc6a612b72f7ce4bd
class12 <mansoorihassan409@gmail.com>
2025-07-11 02:02:29 +0500

    completed

---
+++ lab/app.js
@@ -0,0 +1,83 @@
+// Datatype
+// objects
+// {}
+// Category , entity
+//    []    ,   {}
+
+
+// document.write("Hello From Javascript");
+
+
+// let userName = prompt("Enter your Name;   ");
+
+// alert("Welcome " + userName + "!");
+
+
+// 2 input 2 numbers
+// addition document
+
+// let firstInput = +prompt("Enter first number");
+// let secondInput = +prompt("Enter second number");
+
+// document.write(firstInput + secondInput);
+
+// document.write(+prompt("Enter a number") + +prompt("Enter second number"));
+
+
+// array [] cars
+// Last Car Type change
+
+// let cars = ["Toyota" , "Honda" , "Hundai", "porsche" , "BMW"];
+// let spckCar = cars[4] = "Ferrari";
+// let addCar = cars[5] = "Audi";
+
+
+// console.log(cars);
+
+
+// username
+// hobbies 5
+// Enter your most favourite hobby
+// Hello username your favourite hobby is : hobby
+// Print Hobby
+
+// let userName = prompt("Enter Your Name");
+// let userHobby = prompt("Enter Your Most Favourite Hobby");
+// let userHobby1 = prompt("Enter Your second Hobby");
+// let userHobby2 = prompt("Enter Your third Hobby");
+// let userHobby3 = prompt("Enter Your fourth Hobby");
+// let userHobby4 = prompt("Enter Your fifth Hobby");
+
+// let userInput = [userHobby, userHobby1, userHobby2, userHobby3, userHobby4];
+
+// console.log("Hello " + userName + ", Your favourite hobby is: " + userInput[0]);
+
+// console.log(userInput);
+
+
+
+// let Toyota = {
+//     Model: "Corolla",
+//     Price: "4700000",
+//     Color: "Black",
+// }
+
+// console.log(Toyota);
+
+
+
+// let human = {
+//     name: "Hassan",
+//     fathername: "Muhammad Sharif",
+//     age: "18",
+// }
+
+// console.log(human);
+
+// let human = {
+//     name: "Hassan",
+//     fathername: "Muhammad Sharif",
+//     age: "18",
+// }
+
+// console.log(human.fathername);
