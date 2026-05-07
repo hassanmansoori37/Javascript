ae871fd9b380899b59bea28e22caf703a18d5e7a
class12 <mansoorihassan409@gmail.com>
2026-05-08 02:32:27 +0500

    local & global varriables

---
+++ Class Local varriables & global varriables/app.js
@@ -0,0 +1,59 @@
+
+
+// hello();
+
+
+
+// function hello() {
+//     console.log("Hello Hassan");
+    
+// }
+
+// hello();
+
+
+// hello1();
+// let hello1 = () => {
+// console.log("Hello Hannan");
+
+// }
+
+// hello1();
+
+
+// console.log(user);
+
+// var user = "hassan";
+
+// let newfunc = () => {
+//     let user = "Saylani"
+//     console.log(user);
+    
+// }
+
+// newfunc();
+
+// console.log(user);
+
+// let test = () => {
+//     let user = 1;
+//     console.log(user);
+    
+// }
+
+// test();
+
+// let test2 = () => {
+//     let user = 2;
+//     console.log(user);
+    
+// }
+
+// test2();
+
+// let test3 = () => {
+//     console.log(user);
+    
+// }
+
+// test3();
