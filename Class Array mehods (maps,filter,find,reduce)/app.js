be3b74742d645dc36237eb38a767364751f99504
class12 <mansoorihassan409@gmail.com>
2026-05-11 01:55:38 +0500

    array method

---
+++ Class Array mehods (maps,filter,find,reduce)/app.js
@@ -0,0 +1,53 @@
+// includes
+// map
+// filter
+// find
+// reduce
+// forEach
+
+// let arr = ["yellow" , "green" , "blue"];
+// // console.log(arr.includes("orange"));
+
+// let checker = (str) => {
+//     let color = arr.includes(str);
+
+//     if(color === true){
+//         console.log("your answer is right");
+        
+//     }
+//     else {
+//         console.log("your answer is wrong");
+        
+//     }
+// };
+
+// checker("yellow");
+
+
+let newData = [
+    {
+        name: "hassan",
+        age: 19,
+        id: 1,
+    },
+    {
+         name: "mudassir",
+        age: 22,
+        id: 2,
+
+    }
+];
+
+// let checker = newData.map((data, index) => {
+//     // console.log(data.name);
+// // console.log(index);
+// })
+
+let arr = [5, 10, 15, 20];
+let checker = arr.find((data) => {
+    console.log(data);
+    
+})
+
+    
+
