0f46ccbcf934bfe9726badb4c993689ae4c6b29b
class12 <mansoorihassan409@gmail.com>
2026-05-12 02:20:25 +0500

    array method completed

--- Class Array mehods (maps,filter,find,reduce)/app.js
+++ Class Array mehods (maps,filter,find,reduce)/app.js
@@ -24,30 +24,62 @@
 // checker("yellow");
 
 
-let newData = [
-    {
-        name: "hassan",
-        age: 19,
-        id: 1,
-    },
-    {
-         name: "mudassir",
-        age: 22,
-        id: 2,
+// let newData = [
+//     {
+//         name: "hassan",
+//         age: 19,
+//         id: 1,
+//     },
+//     {
+//          name: "mudassir",
+//         age: 22,
+//         id: 2,
 
-    }
-];
+//     }
+// ];
 
 // let checker = newData.map((data, index) => {
 //     // console.log(data.name);
 // // console.log(index);
 // })
 
-let arr = [5, 10, 15, 20];
-let checker = arr.find((data) => {
-    console.log(data);
-    
-})
+// let arr = [5, 10, 15, 20];
+// let checker = arr.find((data) => {
+//     return data > 10;
+// })
 
-    
+// console.log(checker);
+
+
+// let newData = [
+//     {
+//         name: "hassan",
+//         age: 17,
+//         id: 1,
+//     },
+//     {
+//          name: "mudassir",
+//         age: 22,
+//         id: 2,
+
+//     }
+// ];
+
+// let checker = newData.filter((data) => data.age > 18)
+// console.log(checker);
+
+// console.log(newData);
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let checker = arr.reduce((acc, current) => { 
+//     return acc + current;
+
+// })
+
+// console.log(checker);
+
+
+
+
 
